faaba281d75a19d7e07e7965e927ccc6e2a219b3
Jolene Kearse <pharaohnutz@gmail.com>
2023-07-29 08:31:52 -0400

    chore: set up project scaffold & clear out boilerplate

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export default [
+  {
+      title: "Shenzhen",
+      location: "China",
+      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+      year: "2005-2018",
+      duration: "about 11 years",
+      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  }
+]
