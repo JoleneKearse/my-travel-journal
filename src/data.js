f94930d288337af36df2b823e196085412ce73f7
Jolene Kearse <pharaohnutz@gmail.com>
2023-07-30 09:56:56 -0400

    chore: add data about Korean trips

--- src/data.js
+++ src/data.js
@@ -18,12 +18,103 @@ export default [
   {
     title: "Hong Kong",
     location: "Hong Kong",
-    img: "../public/hongkong.jpg",
+    img: "/hongkong.jpg",
     googleMapsUrl:
       "https://earth.google.com/web/search/hongkong/@22.35283158,114.13948866,193.41224911a,68997.52631225d,35y,0h,0t,0r/data=CnMaSRJDCiUweDM0MDNlMmVkYTMzMjk4MGY6MHhmMDhhYjNiYWRiZWFjOTdjGTn8f-a9UTZAIbtt8M_WilxAKghob25na29uZxgCIAEiJgokCecoaby_xTdAEeBesjmlOzZAGeOycDWe4VxAIQ6XkeGsdFxA",
     year: "2006-2018",
     duration: "many short 1-5 day trips over the years",
     description:
       "Yes, I called Hong Kong its own country! I visited it as a foreigner working in China, where I had to leave the country due to visa requirements. Everytime I crossed the border, I breeved a bref of fresh air! While I loved living in China, it was a relief to visit a city-state that was Westernized. I loved the pub culture, hanging out on the TST waterfront, taking the Star Ferry, and visiting Lantao Island.",
+  },
+  {
+    title: "Iksan",
+    location: "South Korea",
+    img: "/iksan.webp",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Iksan-si,+Jeollabuk-do,+South+Korea/@36.02004245,126.99556401,12.56614483a,60201.26090802d,35y,0h,0t,0r/data=Co4BGmQSXgolMHgzNTcwMTQ4MDhkMzk0MjRkOjB4ZWIxMDM4NTliODUyOGEzYxn02DtI9AJCQCEKod7nU79fQCojSWtzYW4tc2ksIEplb2xsYWJ1ay1kbywgU291dGggS29yZWEYAiABIiYKJAkcTzfyzvlKQBEApQTLv_FKQBneotzKyC78vyG_z75KLSv-vw",
+    year: "2009-2010",
+    duration: "11 months",
+    description:
+      "A small-ish city in the center of South Korea, its status as a railway junction let me explore many beautiful places in this country. I loved visiting museums, sampling different cuisines, and going hiking in this mountainous nation.",
+    personal:
+      "I got to help a brand-new kindergarten establish their English curriculum and got a taste of their hectic test-prep centers.",
+  },
+  {
+    title: "Jeonju",
+    location: "South Korea",
+    img: "/jeonju.avif",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Jeonju-si,+Jeollabuk-do,+South+Korea/@35.81584782,127.11611566,28.01271797a,30193.92766386d,35y,0h,0t,0r/data=Co8BGmUSXwolMHgzNTcwMjM0MzAzMTZkODYzOjB4YzdiYzE3ZTA5MWMzNDI5YRlyCdFr_elBQCHa9LHMbMdfQCokSmVvbmp1LXNpLCBKZW9sbGFidWstZG8sIFNvdXRoIEtvcmVhGAIgASImCiQJ23kLvDA6QkARwneaBXfKQUAZ3Vf9hsXqX0AhzPgzEamUX0A",
+    year: "2009-2010",
+    duration: "frequent day trips",
+    description:
+      "A short hop on the train, I often visited this traditional village.",
+  },
+  {
+    title: "Daejeon",
+    location: "South Korea",
+    img: "/daejeon.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Daejeon,+South+Korea/@36.70940078,127.40071551,177.43265021a,238466.22745356d,35y,360h,0t,0r/data=Cn8aVRJPCiUweDM1NjU0OGQ3YmE0YTY2MDE6MHhkMTk2ZDY5ZDk4OGFkM2I1GeKNzCN_K0JAITaCnhc42V9AKhREYWVqZW9uLCBTb3V0aCBLb3JlYRgCIAEiJgokCT6nEjCPMUJAEVuu84DGEUJAGbbwi5u83l9AIRpEfABDxl9A",
+    year: "2009",
+    duration: "1 day trip, or more?",
+    description: "This was the nearest big city and great for shopping.",
+  },
+  {
+    title: "Seoul",
+    location: "South Korea",
+    img: "/seoul.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Seoul/@37.56481823,126.97416868,32.5759316a,59041.02689576d,35y,0h,0t,0r/data=CnAaRhJACiUweDM1N2NhMjAxMmQ1YzM5Y2Y6MHg3ZTExZWNhMTQwNWJmMjliGQu6GF6kxkJAIT54SIx5v19AKgVTZW91bBgCIAEiJgokCdVS9ZCNskJAES3mh6J2BEJAGRNCsODdDmBAITrCvADilV9AA",
+    year: "2009-2010",
+    duration: "multiple 1-2 day visits after 1 week orientation",
+    description:
+      "Even coming from Shenzhen, the size of this city just blew me away - I can't imagine what it's like for North Koreans! I spent my first week in the country travelling the subway and learning Korean, thanks to the English, Chinese, and Korean naming. I also loved coming here for all the foreign food in Itaewon.",
+  },
+  {
+    title: "Incheon",
+    location: "South Korea",
+    img: "/incheon.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Incheon,+South+Korea/@35.98263366,125.73111577,13.52323625a,963369.58883872d,35y,0h,0t,0r/data=Cn8aVRJPCiUweDM1Nzk2ZjI1OTYxMzgyNDc6MHg3ZDM3ZmQ5MDJjYjc2MTQyGYoON5ZmukJAIe57LxkirV9AKhRJbmNoZW9uLCBTb3V0aCBLb3JlYRgCIAEiJgokCSpaHrK03UJAETitaA7UskJAGW9KGiYmz19AIVRVn3KKrV9A",
+    year: "2009",
+    duration: "3-7 days",
+    description:
+      "Coming to this city after living for a spell in my hometown in Canada, felt so good! I had missed Asia's vibrant and enormous cities. I was reunited with my husband after a few month's absence. Having new cuisines available was so much fun, as well as staying in a 'Love Hotel!'",
+  },
+  {
+    title: "seaside beach",
+    location: "South Korea",
+    img: "/korean_beach.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Haseom+Island,+Buan-gun,+South+Korea/@35.6624203,126.4860136,16.81541788a,839.10600052d,35y,-323.98288142h,45t,0r/data=Co8BGmUSXwolMHgzNTcxMGY2YzNiMzAzNDIzOjB4YWI5YmYzYTY2MjdmMGI0MBlvWjowytRBQCFRWsnYGp9fQCokSGFzZW9tIElzbGFuZCwgQnVhbi1ndW4sIFNvdXRoIEtvcmVhGAIgASImCiQJE6mThpzVQUARdYiYaWDUQUAZ_WB4M6KfX0AhF_4SbtieX0A",
+    year: "2010",
+    duration: "3-4 days",
+    description:
+      "I can't remember the exact name of this one, but I had great memories here. It was an awesome seaside village located on a peninsula that let you walk for miles (it seemed) along the sea. This was rather adventurous of me, as North Korea had just launched missiles in the area and I decided to go anyway!",
+  },
+  {
+    title: "Gochang Dolmen Site",
+    location: "South Korea",
+    img: "/gochang.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Gochang+Dolmen+Site,+Jidong-gil,+Gochang-eup,+Gochang-gun,+Jeollabuk-do,+South+Korea/@0,0,0a,22251752.77375655d,35y,0h,0t,0r/data=CigiJgokCX8O5EyVmEFAEXVY9lU1bEFAGe_wY4yl0V9AIeka62z8r19A",
+    year: "2010",
+    duration: "daytrip",
+    description:
+      "I was so excited to see this park with stone dolmens built by prehistoric people! Like who wouldn't want to see that!?! Unfortunately, I took a wrong turn before I could see much and ended up hiking way out into the country. I did meet a friendly farming family that called a taxi to pick up the way-ward tourists.",
+  },
+  {
+    title: "Ilkley",
+    location: "England",
+    img: "/ilkley.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Ilkley,+UK/@53.92016274,-1.82348157,147.7412015a,10961.10141687d,35y,0h,0t,0r/data=CnUaSxJFCiUweDQ4N2JlMmQ2MGYyNzhiMmQ6MHg0OWNhMDMwOWI2OWRmNmMwGfNzQ1N29kpAIT18mShCKv2_KgpJbGtsZXksIFVLGAIgASImCiQJ6VEUEJGMNkARD4wC2vwnNkAZkztQI7iYXEAh3jJgoCJ5XEA",
+    year: "2008",
+    duration: "4 months",
+    description:
+      "A small town in Northern England that actually has higher property prices than London! I went hiking a lot, especially to the Ilkley Moor and struggled to drive on the left side of their excessively tiny roads.",
+    personal:
+      "While I got to see lots of nearby areas, my inability to get a working visa compelled me to move on.",
   },
 ];
