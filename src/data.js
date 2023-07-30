0e5f740ec9a55b2e05531261cc68bc0f73a20d58
Jolene Kearse <pharaohnutz@gmail.com>
2023-07-30 17:56:19 -0400

    chore: add more travels

--- src/data.js
+++ src/data.js
@@ -1,6 +1,3 @@
-// import shenzhen from "/shenzhen.jpg"
-// import hongkong from "/hongkong.jpg"
-
 export default [
   {
     title: "Shenzhen",
@@ -9,8 +6,8 @@ export default [
     googleMapsUrl:
       "https://earth.google.com/web/search/Shenzhen,+Guangdong+Province,+China/@22.55516007,114.05387751,83.76628774a,68799.3974141d,35y,0h,0t,0r/data=CigiJgokCXMktY9B8kNAEXEktY9B8kPAGSbWwrXPMkhAIVcVGWQ6yFDA",
     coordinates: "22°32′29″N 114°03′35″E",
-    year: "2004-2018",
-    duration: "about 11 years",
+    year: "2006-2018",
+    duration: "altogether about 12 years",
     description:
       "Shenzhen became my beloved Chinese hometown - I even moved back home after some travels! It was a great place to meet people from all over the world and from other areas in the mainland since it's a migrant city. What started out as a collection of small villages spawned into a metropolis of around 20 million (unoffically). I loved the small town feel of the Shekou neighbourhood, where all the expats knew each other. And how you could use the extensive public transportation system to reach the giant city with it's LED-lite skyscrappers with ease.",
     personal:
@@ -150,5 +147,127 @@ export default [
     duration: "2 day trips, I think",
     description:
       "A nice day trip to a seaside resort on the Irish Sea. I loved seeing sites I'd only known from TV shows, especially the amusement park and pier.",
+  },
+  {
+    title: "Harrogate",
+    location: "England",
+    img: "/harrogate.avif",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Harrogate,+UK/@53.9940165,-1.52891475,124.23089306a,10932.97255908d,35y,0h,0.00015836t,0r/data=CncaTRJHCiQweDQ4Nzk0ZTIzNzE3NWJkMDE6MHgzYzA4NGZiYWFkZWE0ZmYZkNrEyf3-SkAhH0lJD0Or-L8qDUhhcnJvZ2F0ZSwgVUsYAiABIiYKJAk0NR_mpLhBQBGzg5c7nrdBQBkiSjt0a6pfQCFL5iYrlalfQA",
+    coordinates: "53°59′31″N 01°32′16″W",
+    year: "2008",
+    duration: "a few day trips",
+    description:
+      "East of the Yorkshire Dales, I mainly remember this as a cute town with plenty of tea shops.",
+  },
+  {
+    title: "York",
+    location: "England",
+    img: "/york.webp",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/York,+UK/@53.95859682,-1.08059562,12.66506314a,10946.38084535d,35y,0h,0t,0r/data=CnMaSRJDCiUweDQ4NzhjMzQwZTE5ODY1ZjE6MHg0Nzc0YWI4OThhNTRlNGQxGRuastMP-0pAIZ8jTRe9LvG_KghZb3JrLCBVSxgCIAEiJgokCePfDss-A0tAEf2Mr303-0pAGcoJoNrjPfe_IZzdk8X6rvm_",
+    coordinates: "53.960106°N 1.078017°W",
+    year: "2008",
+    duration: "2 or 3 day trips",
+    description:
+      "This was my favourite place to visit in England. The narrow, twisty streets are just like you'd imagine a medieval village to look and feel like. It has a still-standing city wall that you can walk around. And York Minster's architecture is amazing.",
+  },
+  {
+    title: "Guangzhou",
+    location: "China",
+    img: "/guangzhou.webp",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Guangzhou,+Guangdong+Province,+China/@23.12561575,113.22784679,1.76320821a,136901.92697314d,35y,0h,0t,0r/data=Co4BGmQSXgokMHgzNDAyZjg5NWEzNWMyYmM3OjB4ZTU5ZTA3NWFkZWFlNDE1GSoSXWELITdAIbeXNEbrUFxAKiRHdWFuZ3pob3UsIEd1YW5nZG9uZyBQcm92aW5jZSwgQ2hpbmEYAiABIiYKJAmIwCBs7L8-QBHo6g7_xWI-QBm2CUzQlqZcQCEb04Uv3H9cQA",
+    coordinates: "23°07′48″N 113°15′36″E",
+    year: "2006-2017",
+    duration: "numerous overnight and day trips",
+    description:
+      "Another Chinese metropolis, but, like, of course, it's the historical Canton! I made many trips over the years, but I really enjoyed Shamian Island as a retreat within the city.",
+  },
+  {
+    title: "Macau",
+    location: "autonomous region of China",
+    img: "/macau.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/macau/@22.16183201,113.57015267,6.51469553a,17230.38440428d,35y,0h,0t,0r/data=CnAaRhJACiUweDM0MDE3YWUwZTIzNWM4ZjM6MHhjNjdiZTMyY2I0ODVhY2Y2GVc-y_PgMjZAIVoLs9DOYlxAKgVtYWNhdRgCIAEiJgokCetkxKis_kpAEVLz9F649kpAGVF9Lya3FPC_Ifkkm699f_K_",
+    coordinates: "22°10′N 113°33′E",
+    year: "2007-2008",
+    duration: "2 short trips",
+    description:
+      "Simply the most exotic place I've ever been! This former Portugeuse colony has gorgeous buildings, tiled pavements, and street names mixing Cantonese and Portugeuse. It's a very walkable city, although the numerous hills are challenging on a hot day. The funniest thing though are the 'dog bathrooms' scattered about.",
+  },
+  {
+    title: "Chengdu",
+    location: "China",
+    img: "/chengdu.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Chengdu,+Sichuan,+China/@30.65852864,104.07554912,493.43772659a,64036.30773344d,35y,0h,0t,0r/data=CoIBGlgSUgolMHgzNmVmYzUyMzAwNDQ3NzIxOjB4Yjk4NjUyY2UyZTI0MGUwMhnuT_Off5I-QCEOV72yQQRaQCoXQ2hlbmdkdSwgU2ljaHVhbiwgQ2hpbmEYAiABIiYKJAnjMngLuJU_QBFefmFJbNw-QBnaZdXTX4VeQCGGvYRopjdeQA",
+    coordinates: "30°39′36″N 104°03′48″E",
+    year: "2005",
+    duration: "1 week",
+    description:
+      "This was the furthest West I got in China. It even had an awesome market with many Tibetan stalls! It had the most laidback vibe of any Chinese city I've been to. I loved the juxtaposition between the still-standing and preserved ancient buildings and modern skyscrappers. It's a must go for anyone that wants to see pandas thanks to the nearby research center. You can even get a picture taken with a Giant Panda beside you on a couch.",
+  },
+  {
+    title: "Leshan",
+    location: "China",
+    img: "/leshan.jpeg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Leshan,+Sichuan,+China/@29.57951569,103.75895848,367.98735707a,32383.80820522d,35y,0h,0t,0r/data=CoEBGlcSUQolMHgzNmU4YTBkMDA4NjAxNjU5OjB4NDk3ZjliODNjMzY5MjQyYRkFB8WgXY09QCHeq1Ym_PBZQCoWTGVzaGFuLCBTaWNodWFuLCBDaGluYRgCIAEiJgokCaHG4OFf1z5AEUWlsVGceT5AGQKuLH1YGFpAIbU8HwNT8VlA",
+    coordinates: "29°32′41″N 103°46′24″",
+    year: "2005",
+    duration: "day trip",
+    description:
+      "Home of the Giant Buddha, this was an awesome trip. You can even walk up the side of the 233 ft statue carved in the cliff face. Although, I recommend going outside of national holidays, because it was way too claustrophobic for me!",
+  },
+  {
+    title: "Shanghai",
+    location: "China",
+    img: "/shanghai.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Shanghai,+China/@31.22434809,121.47674348,17.15519709a,127304.07892927d,35y,0h,0t,0r/data=CnkaTxJJCiQweDM1YjI3MDQwYjFmNTNjMzM6MHgyOTUxMjk0MjNjMzY0YTEZonkAi_w6P0AhR8X_HVFeXkAqD1NoYW5naGFpLCBDaGluYRgCIAEiJgokCQ7468oYrD1AEXNOejyQfD1AGS4p1s5Y-llAIa9qHZzM5llA",
+    coordinates: "31°13′43″N 121°28′29″E",
+    year: "2005",
+    duration: "2 trips totaling 10 days",
+    description:
+      "My first trip was explosive! It was during the Chinese New Year and I was able to set off a firework from a mortar shell. I did get a little burnt, but thankfully I was wearing a thick winter coat. I met so many cool people on that trip, that I went back. The second trip was full of laughter. First I was meeting 2 friends there, and although we all arrived at about the same time, we didn't find each other at the hotel for hours. The proprietor was trying to get us all to pay for an individual room! I also have good memories of singing along with Elvis Presley songs and my friend getting sung 'Happy Birthday!' to (not his birthday) at the Hooters after we finally found each other that night.",
+  },
+  {
+    title: "Wuhan",
+    location: "China",
+    img: "/wuhan.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/wuhan/@30.56811634,114.30038817,12.17504694a,64076.15670928d,35y,0h,0t,0r/data=CnAaRhJACiUweDM0MmVhZWY4ZGQ4NWYyNmY6MHgzOWMyYzlhYzZjNTgyMjEwGVbh4By_lz5AIajGSzeJk1xAKgV3dWhhbhgCIAEiJgokCeMyeAu4lT9AEV5-YUls3D5AGdpl1dNfhV5AIYa9hGimN15A",
+    coordinates: "30°35′36″N 114°18′17″E",
+    year: "2004-2005",
+    duration: "1 school year",
+    description:
+      "Yes, I lived in the city at the center of the pandemic! But, don't worry, I moved away in 2005. This was my first Chinese city, one that straddles the Yangtze River. It was also the place where I really realized how many people there are in China! I went to the Walmart one Saturday. First big difference was that it was on multiple floors. I remember feeling claustrophobic and walking around and around the fourth floor desperately looking for an exit!",
+    personal:
+      "My first job as an English as a Foreign Language Teacher was a Hubei University teaching History, and English. I remember my first day as a teacher... I walked into the classroom with 30 eager students and thought: 'What the @#*!$ did I get myself into?'",
+  },
+  {
+    title: "Beijing",
+    location: "China",
+    img: "/beijing.jpg",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/beijing/@39.93887911,116.39745802,52.25191182a,114129.16024214d,35y,0h,0t,0r/data=CigiJgokCahMcGOBgzdAEeuL6sE1vDZAGUtHIZ5pdVxAIVqDjGHAJ1xA",
+    coordinates: "39°54′24″N 116°23′51″E",
+    year: "2005",
+    duration: "1 week",
+    description:
+      "It is a must-see, so I did, but was definitely not my favourite place in China. I did see all the sites though, like the Forbidden City (and was disappointed the Starbucks was closed), The Summer Palace (where I joined all the other tourists climbing the sign saying don't skate on the ice to skate on the ice), and, of course, The Great Wall. I kept running up and up and up, but could never reach the top! I also got my picture taken beside the 'Now you are a real man' sign and thus became a real man.",
+  },
+  {
+    title: "Yangshou",
+    location: "China",
+    img: "/yangshou.webp",
+    googleMapsUrl:
+      "https://earth.google.com/web/search/Yangshuo,+Guilin,+Guangxi,+China/@24.84755717,110.45393173,274.14716039a,67538.49129523d,35y,0h,0t,0r/data=CosBGmESWwolMHgzNmE1YTU3ZTExOTU1MjY1OjB4NDcwYmU0NThhZWVlMDAwNRmCupIuR8c4QCGZDTLJyJ9bQCogWWFuZ3NodW8sIEd1aWxpbiwgR3Vhbmd4aSwgQ2hpbmEYAiABIiYKJAnInsklOwQ3QBGsBBvZaqA2QBlQLoW5riVbQCHtWL7G7v5aQA",
+    coordinates: "24°47′N 110°30′E",
+    year: "2005",
+    duration: "1 week",
+    description:
+      "This was The China I had always imagined! The karst mountains around town and ever present fog were stunning. ",
   },
 ];
